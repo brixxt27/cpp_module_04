c4ab6600c44a8069f1ba35bbf8b3a025737af18e
“brixxt27” <brixxt27@gmail.com>
2023-01-11 20:15:29 +0900

    feat: AMateria 클래스 구현#17
    use 메소드를 위해 target인 ICharacter의 캐릭터 이름이 필요하다. 그러나
    이는 인터페이스라 멤버 변수가 없다. 때문에 이를 알기 위해 concrete
    class인 Character를 구현해야 한다.

--- ex03/AMateria.cpp
+++ ex03/AMateria.cpp
@@ -6,10 +6,36 @@ AMateria::AMateria(std::string const& type)
 	std::cout << "AMateria's constructor is called" << std::endl;
 }
 
+AMateria::AMateria(const AMateria& other)
+	: _type(other._type)
+{
+	std::cout << "AMateria's copy constructor is called" << std::endl;
+}
 
+AMateria&	AMateria::operator=(const AMateria& rhs)
+{
+	if (this != &rhs) {
+		_type = rhs._type;
+	}
+	return *this;
+}
 
+AMateria::~AMateria()
+{
+	std::cout << "AMataria's destructor is called" << std::endl;
+}
 
 AMateria::AMateria()
 {
 	std::cout << "AMateria's default constructor is called" << std::endl;
 }
+
+std::string const & AMateria::getType() const
+{
+	return _type;
+}
+
+//void	AMateria::use(ICharacter& target)
+//{
+
+//}
