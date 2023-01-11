6a76b6f1a3288732f89efffdc02f07b3164b85df
“brixxt27” <brixxt27@gmail.com>
2023-01-11 14:36:31 +0900

    feat: Cat에도 깊은 복사 구현 완료 #8 #13
    따로 테스트가 필요하다. 유효한지에 대한 테스트는 하지 않음. main 작성
    시에 할 예정이다

--- ex01/Cat.cpp
+++ ex01/Cat.cpp
@@ -16,6 +16,14 @@ Cat::Cat(const Cat& other)
 	: Animal(other._type)
 {
 	std::cout << "Cat's copy constructor is called" << std::endl;
+	
+	_brain = new (std::nothrow) Brain();
+	if (_brain == NULL) {
+		std::cout << "Memory allocation is fail" << std::endl;
+		std::exit(1);
+	}
+
+	_brain->SetIdeas((other._brain->GetIdeas())[0]);
 }
 
 Cat&	Cat::operator=(const Cat& rhs)
@@ -25,6 +33,15 @@ Cat&	Cat::operator=(const Cat& rhs)
 	if (this != &rhs)
 	{
 		_type = rhs._type;
+
+		delete _brain;
+		_brain = new (std::nothrow) Brain();
+		if (_brain == NULL) {
+			std::cout << "Memory allocation is fail" << std::endl;
+			std::exit(1);
+		}
+
+		_brain->SetIdeas(*(rhs._brain->GetIdeas()));
 	}
 
 	return *this;
@@ -41,4 +58,9 @@ Cat::~Cat()
 void	Cat::makeSound() const
 {
 	std::cout << "Meow!" << std::endl;
+}
+
+Brain*	Cat::GetBrain() const
+{
+	return _brain;
 }
